fae37cb020b7701c3c9a4cfe22edc47f38f21f77
Сергей Бутаков <xolodnotchk@mail.ru>
2024-04-13 17:25:43 +0300

    добавление класса Vacancy

---
+++ src/vacancy.py
@@ -0,0 +1,22 @@
+from src.abstract import API
+
+
+class Vacancy(API):
+    """ Класс для работы с вакансиями. """
+    name: str   # Название вакансии
+    url_job: str    # Ссылка на вакансию
+    description: str   # Описание вакансии
+    experience: str   # Опыт работы
+    work_schedule: str   # График работы
+    pay: float   # Зарплата
+
+    def __init__(self, name, url_job, description, experience, work_schedule, pay):
+        self.name = name
+        self.url_job = url_job
+        self.description = description
+        self.experience = experience
+        self.work_schedule = work_schedule
+        self.pay = pay
+
+    def comparison_vacation(self):
+        """ Метод, который сравнивает вакансии. """
