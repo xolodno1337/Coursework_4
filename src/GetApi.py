2af505869b6b7d13c95ed5a12a9e16de360fed7e
Сергей Бутаков <xolodnotchk@mail.ru>
2024-04-17 17:03:31 +0300

    доработка класса для получения вакансий api сервиса

---
+++ src/GetApi.py
@@ -0,0 +1,27 @@
+import requests
+from src.AbstractAPI import AbstractApi
+
+
+class GetAPI(AbstractApi):
+    """ Класс для работы с API сервиса с вакансиями. """
+
+    def __init__(self, name_vacancy):
+        self.all_vacancy = {}
+        self.url_get = 'https://api.hh.ru/vacancies/'
+        self.data = {'text': name_vacancy, 'page': 1, 'only_with_salary': 'true'}
+
+    def __str__(self):
+        """ Представление класса для пользователей. """
+        return 'Класс для работы с API сервиса с вакансиями.'
+
+    def get_vacancy(self):
+        """ Подключение к API сервису с вакансиями. """
+        response = requests.get(self.url_get, params=self.data, headers={'Content-Type': 'application/json; '
+                                                                                         'charset=utf-8'})
+        return response.json()
+        # self.all_vacancy['items'] = json.loads(response.text)['items']
+        # for vacancy in self.all_vacancy['items']:
+        #     if name_vacancy in (vacancy['name']):
+        #         return vacancy
+        #     else:
+        #         print('Нет такой вакансии')
