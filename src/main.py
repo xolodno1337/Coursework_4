8f28740822bb3fe4fd5ff2233c5091763d5f9cea
Сергей Бутаков <xolodnotchk@mail.ru>
2024-04-23 16:33:10 +0300

    добавление функций

--- src/main.py
+++ src/main.py
@@ -1,14 +1,40 @@
 from src.GetApi import GetAPI
 from src.GetJson import GetJson
+from src.Vacancy import Vacancy
+from src.func import filter_word, sorted_salary, get_top
 
 
 def user_interaction():
     """Функция для взаимодействия с пользователем """
     platforms = ["HeadHunter"]
-    search_query = input("Введите поисковый запрос: ")
+    search_query = input("Введите поисковый запрос: ").title()
+    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").title().split()
+    salary_range = list(map(int, input("Введите диапазон зарплат, пример: 100000 - 150000: ").split('-')))
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    salary_range = input("Введите диапазон зарплат: ")   # Пример: 100000 - 150000
+
     vacancy = GetAPI(search_query)
     vacancy_json = GetJson()
     vacancy_json.save_vacancy(vacancy.get_vacancy())
+    list_vacancy = [
+        Vacancy(
+            name=i['name'],
+            url_job=i['url'],
+            requirement=i['snippet']['requirement'] if i['snippet']['requirement'] else 'Нет описания',
+            description=i['snippet']['responsibility'] if i['snippet']['responsibility'] else 'Нет описания',
+            experience=i['experience']['name'],
+            work_schedule=i['employment']['name'],
+            pay_from=i['salary']['from'] if i['salary']['from'] else i['salary']['to'],
+            pay_to=i['salary']['to'] if i['salary']['to'] else i['salary']['from'])
+        for i in vacancy_json.read_vacancy()['items']]
+
+    f_vacancy = filter_word(filter_words, list_vacancy)
+    i = sorted_salary(salary_range, f_vacancy)
+    v = get_top(top_n, f_vacancy)
+
+
+user_interaction()
+# while True:
+#     command = input("Введите команду 1 для сортировки по зарплате, 2 - для вывода в топ): ")
+#     if command == "1":
+#         вызываешь функцию сортировки
+#     elif command == "2": вызываешь функцию для вывода в топ
